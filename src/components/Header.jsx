e5585f0e99ffbcd7b0133a9b7b388dae44e59712
Hernandez Maikol <hernandezdev@proton.me>
2024-01-29 16:09:05 -0400

    Components - Header - creating component basic layout

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header(props) {
+  return(
+    <header className="flex">
+      <h1 className="leading-none">
+        logo
+      </h1>
+      <button className="block">
+        theme switcher
+      </button>
+    </header>
+  )
+}
