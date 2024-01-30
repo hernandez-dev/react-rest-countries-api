9edd11e10d420375cfd99e125f511120e8c60f89
Hernandez Maikol <hernandezdev@proton.me>
2024-01-30 17:11:12 -0400

    Components - Header - wrapping main heading into Link component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
+import { Link } from "react-router-dom"
+
 export default function Header({ darkMode, dispatch }) {
   return (
     <header className="flex items-center justify-between p-6 px-10 shadow-sm shadow-black/[15%]">
-      <h1 className="text-2xl capitalize leading-none">
-        where in the world!
-      </h1>
+      <Link to="/">
+        <h1 className="text-2xl capitalize leading-none">
+          where in the world!
+        </h1>
+      </Link>
       <button className="block capitalize">
         dark mode
       </button>
