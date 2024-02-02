51511cb0da2440b27dc575ea7caa19b8ec7e43e8
Hernandez Maikol <hernandezdev@proton.me>
2024-02-02 12:05:49 -0400

    Project - App.jsx - implementing basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+// components
+import Header from "./components/Header.jsx"
+import Countries from "./components/Countries.jsx"
+import CountryDetails from "./components/CountryDetails.jsx"
 
 function App() {
   return (
-   <h1 className="text-red-200 text-center">
-      welcome
-    </h1> 
-  )
+    <BrowserRouter>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Countries route="/all" />} />
+          <Route path="/region/:region" element={<Countries route={`/region/oceania`} />} />
+          <Route path="/:code/details" element={<CountryDetails />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
 }
 
 export default App
