2e4b43a7eea64a8f1f302a2b9834e56a50a77266
Hernandez Maikol <hernandezdev@proton.me>
2024-02-16 10:46:12 -0400

    Routes - countries - creating route basic setup

---
+++ src/routes/countries.jsx
@@ -0,0 +1,34 @@
+import { useLoaderData } from "react-router-dom"
+
+// components
+import Page from "../components/Page.jsx"
+import CountryCard from "../components/CountryCard.jsx"
+
+// api
+import { fetchRequest } from "../api.js"
+
+// loader
+export async function loader() {
+  try {
+    const response = await fetchRequest('/all')
+    return response
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+export default function Countries() {
+  // countries
+  const countries = useLoaderData()
+
+  return(
+    <Page title="Where in the world!">
+      <header className="p-10 pb-0">
+        search box and region selector will go here
+      </header>
+      <div className="grid grid-cols-4 gap-5 p-10">
+        {countries.map(country => <CountryCard key={country.name.common} country={country} />).slice(0, 50)}
+      </div>
+    </Page>
+  )
+}
