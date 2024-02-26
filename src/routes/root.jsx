1aed1877f2caa3cd0c50a6634adf877d74b1d442
Hernandez Maikol <hernandezdev@proton.me>
2024-02-26 12:29:41 -0400

    Routes - root - adding logic to handle app dark mode

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,12 +1,28 @@
+import { useEffect } from "react"
+import { useImmer } from "use-immer"
 import { Outlet } from "react-router-dom"
 
 // components
 import Header from "../components/Header.jsx"
 
 export default function Root() {
+  // state
+  const [state, setState] = useImmer({ darkMode: localStorage.getItem("darkMode") })
+
+  // darkMode changes
+  useEffect(() => {
+    if (state.darkMode) {
+      document.documentElement.classList.add("dark")
+      localStorage.setItem("darkMode", 1)
+    } else {
+      document.documentElement.classList.remove("dark")
+      localStorage.removeItem("darkMode")
+    }
+  }, [state.darkMode])
+
   return(
     <>
-      <Header />
+      <Header setState={setState} />
       <Outlet />
     </>
   )
