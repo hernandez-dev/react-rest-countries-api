e267f70ff2b6688c37a077c4387a97cf8602b84a
Hernandez Maikol <hernandezdev@proton.me>
2024-02-26 13:25:47 -0400

    Components - Header - wrapping content into a div to handle max width

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,15 +2,17 @@ import { Link } from "react-router-dom"
 
 export default function Header({ setState }) {
   return (
-    <header className="flex items-center justify-between p-6 px-10 shadow-sm shadow-black/[15%] text-primary-dark dark:text-white">
-      <Link to="/">
-        <h1 className="text-2xl capitalize leading-none">
-          where in the world!
-        </h1>
-      </Link>
-      <button onClick={() => setState(draft => { draft.darkMode = !draft.darkMode })} className="block capitalize">
-        dark mode
-      </button>
+    <header className="p-6 px-10 shadow-sm shadow-black/[15%] text-primary-dark dark:text-white desktop:px-0">
+      <div className="flex items-center justify-between max-w-desktop mx-auto">
+        <Link to="/">
+          <h1 className="text-2xl capitalize leading-none">
+            where in the world!
+          </h1>
+        </Link>
+        <button onClick={() => setState(draft => { draft.darkMode = !draft.darkMode })} className="block capitalize">
+          dark mode
+        </button>
+      </div>
     </header>
   )
 }
