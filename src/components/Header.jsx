29265f639ff6ec9673b820e73619f8fc5adb99d8
Hernandez Maikol <hernandezdev@proton.me>
2024-02-26 14:34:02 -0400

    Components - Header - handling inner container media rules

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom"
 
 export default function Header({ setState }) {
   return (
-    <header className="p-6 px-10 shadow-sm shadow-black/[15%] text-primary-dark dark:text-white desktop:px-0">
-      <div className="flex items-center justify-between max-w-desktop mx-auto">
+    <header className="shadow-sm shadow-black/[15%] text-primary-dark dark:text-white">
+      <div className="flex items-center justify-between p-6 px-5 max-w-2xl mx-auto tablet:max-w-full countries-desktop:px-10 desktop:max-w-desktop desktop:px-0">
         <Link to="/">
           <h1 className="text-2xl capitalize leading-none">
             where in the world!
