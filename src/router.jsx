43fe81632b6fde406bb9af2dded8de604ca10996
Hernandez Maikol <hernandezdev@proton.me>
2024-03-07 17:27:23 -0400

    Project - router - adding new route ( region/:region )

--- src/router.jsx
+++ src/router.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from "react-router-dom"
 
 // routes
 import Root from "./routes/root.jsx"
-import Countries, { loader as countriesLoader } from "./routes/countries.jsx"
+import Countries, { loader as countriesLoader } from "./routes/countries/index.jsx"
 import CountryDetails, { loader as countryLoader } from "./routes/country-details.jsx"
 
 const router = createBrowserRouter([
@@ -12,6 +12,11 @@ const router = createBrowserRouter([
     children: [
       {
         path: "",
+        loader: countriesLoader,
+        element: <Countries />
+      },
+      {
+        path: "region/:region",
         loader: countriesLoader,
         element: <Countries />
       },
