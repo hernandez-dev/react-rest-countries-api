2d136a95541f2a65e3a8bf4eea2d0e2433278508
Hernandez Maikol <hernandezdev@proton.me>
2024-03-08 15:49:09 -0400

    Components - Header - changing heading font weight

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header({ setState }) {
     <header className="shadow-sm shadow-black/[15%] text-dark-blue dark:bg-dark-blue dark:shadow-black/70 dark:text-white">
       <div className="flex items-center justify-between p-6 px-5 max-w-2xl mx-auto tablet:max-w-[1100px] countries-desktop:max-w-full countries-desktop:px-10 desktop:max-w-desktop desktop:px-0">
         <Link to="/">
-          <h1 className="text-2xl capitalize leading-none">
+          <h1 className="font-bold text-2xl capitalize leading-none">
             where in the world!
           </h1>
         </Link>
