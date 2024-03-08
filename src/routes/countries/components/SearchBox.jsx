095ab07f34dbc8d16c0c34f2db69dc88e2cf63d1
Hernandez Maikol <hernandezdev@proton.me>
2024-03-08 15:02:17 -0400

    Routes - countries/components - improving search box layout

--- src/routes/countries/components/SearchBox.jsx
+++ src/routes/countries/components/SearchBox.jsx
@@ -2,8 +2,8 @@ import { Form } from "react-router-dom"
 
 export default function SearchBox() {
   return(
-    <Form className="flex items-center bg-white rounded-md shadow-md text-dark-blue overflow-hidden dark:bg-dark-blue dark:text-white" role="search">
-      <span className="block w-[3rem] h-[3rem] ml-1 text-center leading-[3rem]">
+    <Form className="flex items-center bg-white rounded-md shadow-md text-dark-gray overflow-hidden dark:bg-dark-blue dark:text-white" role="search">
+      <span className="block w-[3.5rem] h-[3.5rem] ml-1 text-center leading-[3.5rem]">
         <i className="fa-solid fa-search"></i>
       </span>
       <input
