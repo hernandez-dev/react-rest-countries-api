3e1ae574aa65c9533d14ee3f3c956120a998a7a4
Hernandez Maikol <hernandezdev@proton.me>
2024-03-08 15:24:23 -0400

    Components - Header - limiting inner container using countries media rules

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 export default function Header({ setState }) {
   return (
     <header className="shadow-sm shadow-black/[15%] text-dark-blue dark:bg-dark-blue dark:shadow-black/70 dark:text-white">
-      <div className="flex items-center justify-between p-6 px-5 max-w-2xl mx-auto tablet:max-w-full countries-desktop:px-10 desktop:max-w-desktop desktop:px-0">
+      <div className="flex items-center justify-between p-6 px-5 max-w-2xl mx-auto tablet:max-w-[1100px] countries-desktop:max-w-full countries-desktop:px-10 desktop:max-w-desktop desktop:px-0">
         <Link to="/">
           <h1 className="text-2xl capitalize leading-none">
             where in the world!
