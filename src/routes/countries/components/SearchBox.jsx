bd250c78a5a80f4f8646efe4fbcc9c09c1f2e5eb
Hernandez Maikol <hernandezdev@proton.me>
2024-03-08 11:16:48 -0400

    Routes - countries/components - adding search box icon

--- src/routes/countries/components/SearchBox.jsx
+++ src/routes/countries/components/SearchBox.jsx
@@ -3,8 +3,8 @@ import { Form } from "react-router-dom"
 export default function SearchBox() {
   return(
     <Form className="flex items-center bg-white rounded-md shadow-md text-dark-blue overflow-hidden dark:bg-dark-blue dark:text-white" role="search">
-      <span className="block w-[3rem] h-[3rem] text-center leading-[3rem]">
-        i
+      <span className="block w-[3rem] h-[3rem] ml-1 text-center leading-[3rem]">
+        <i className="fa-solid fa-search"></i>
       </span>
       <input
         id="search"
