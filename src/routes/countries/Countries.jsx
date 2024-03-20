e5d7d9177bf964dc008234438abb5d3c27857a43
Hernandez Maikol <hernandezdev@proton.me>
2024-03-20 12:52:12 -0400

    Routes - countries - importing and rendering Layout at Countries

--- src/routes/countries/Countries.jsx
+++ src/routes/countries/Countries.jsx
@@ -1,10 +1,7 @@
 import { useLoaderData, useNavigation } from "react-router-dom"
 
 // components
-import Page from "../../components/Page.jsx"
-import CountryCard from "../../components/CountryCard.jsx"
-import SearchBox from "./components/SearchBox.jsx"
-import RegionSelector from "./components/RegionSelector.jsx"
+import Layout from "./components/Layout.jsx"
 
 // api
 import { fetchRequest } from "../../api.js"
@@ -35,22 +32,7 @@ export default function Countries() {
   const navigation = useNavigation()
 
   return(
-    <Page title="Where in the world!">
-      <div className="p-5 space-y-10 max-w-2xl mx-auto tablet:max-w-[1100px] countries-desktop:px-10 countries-desktop:max-w-full desktop:max-w-desktop desktop:px-0">
-        <header className="space-y-5 tablet:flex tablet:items-center tablet:justify-between tablet:space-y-0">
-          <SearchBox />
-          <RegionSelector />
-        </header>
-        {countries ? (
-          <div className={`grid gap-5 ${navigation.state === "loading" ? 'opacity-50' : ''} transition duration-300 tablet:grid-cols-2 countries-desktop:grid-cols-3 countries-xl:grid-cols-4`}>
-            {countries.map(country => <CountryCard key={country.name.common} country={country} />).slice(0, 50)}
-          </div>
-        ) : (
-          <h2 className="text-center text-dark-gray">
-            No results for your search
-          </h2>
-        )}
-      </div>
-    </Page>
+    <Layout title="all countries!" countries={countries}>
+    </Layout>
   )
 }
