76992a2cd820dff0a1f29be92bece54f4a39346d
Hernandez Maikol <hernandezdev@proton.me>
2024-03-20 13:39:48 -0400

    Routes - countries/components - handling SearchBox logic
    At this commit we convert the input to a controlled one. Also,
    we've created a handleSubmit logig to handle the form submission.
    Preventing browser defualt behavior and redirecting when needed.

--- src/routes/countries/components/SearchBox.jsx
+++ src/routes/countries/components/SearchBox.jsx
@@ -1,8 +1,22 @@
-import { Form } from "react-router-dom"
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
 
 export default function SearchBox() {
+  // navigate
+  const navigate = useNavigate()
+
+  // state
+  const [countryName, setCountryName] = useState("")
+
+  // handleSubmit
+  function handleSubmit(e) {
+    e.preventDefault()
+    if (!countryName.trim()) return
+    navigate(`/search-countries/${countryName.toLowerCase()}`)
+  }
+
   return(
-    <Form className="flex items-center bg-white rounded-md shadow-md text-dark-gray overflow-hidden tablet:w-[400px] dark:bg-dark-blue dark:text-white" role="search">
+    <form onSubmit={handleSubmit} className="flex items-center bg-white rounded-md shadow-md text-dark-gray overflow-hidden tablet:w-[400px] dark:bg-dark-blue dark:text-white" role="search">
       <span className="block w-[3.5rem] h-[3.5rem] ml-1 text-center leading-[3.5rem]">
         <i className="fa-solid fa-search"></i>
       </span>
@@ -14,7 +28,9 @@ export default function SearchBox() {
         placeholder="search for a country"
         className="outline-none block w-full bg-transparent capitalize leading-[3rem]"
         autoComplete="off"
+        value={countryName.trim() ? countryName : ''}
+        onChange={e => setCountryName(e.target.value)}
       />
-    </Form>
+    </form>
   ) // component return end
 }
