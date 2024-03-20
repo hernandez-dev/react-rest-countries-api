a74b8d148490d2e204f3fa07a0bf34896e68e4d5
Hernandez Maikol <hernandezdev@proton.me>
2024-03-20 13:05:15 -0400

    Routes - countries/ creating new component SearchCountries

---
+++ src/routes/countries/SearchCountries.jsx
@@ -0,0 +1,31 @@
+import { useLoaderData, useNavigation } from "react-router-dom"
+
+// components
+import Layout from "./components/Layout.jsx"
+
+// api
+import { fetchRequest } from "../../api.js"
+
+// loader
+export async function loader({ request, params }) {
+  try {
+    const response = await fetchRequest(`/name/${q}`)
+    return response
+  } catch (e) {
+    console.log(e)
+    return e
+  }
+}
+
+export default function Countries() {
+  // countries
+  const countries = useLoaderData()
+
+  // navigation
+  const navigation = useNavigation()
+
+  return(
+    <Layout title="all countries!" countries={countries}>
+    </Layout>
+  )
+}
