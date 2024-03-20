601969bedbed6772d225b63c59705e1a7ba32881
Hernandez Maikol <hernandezdev@proton.me>
2024-03-20 12:52:52 -0400

    Routes - countries/components - creating countries common layout component

---
+++ src/routes/countries/components/Layout.jsx
@@ -0,0 +1,28 @@
+import { useNavigation } from "react-router-dom"
+
+// global components
+import Page from "../../../components/Page.jsx"
+
+// components
+import CountryCard from "./CountryCard.jsx"
+import SearchBox from "./SearchBox.jsx"
+import RegionSelector from "./RegionSelector.jsx"
+
+export default function Layout({ title, countries }) {
+  // navigation
+  const navigation = useNavigation()
+
+  return(
+    <Page title={title} scroll={true}>
+      <div className="p-5 space-y-10 max-w-2xl mx-auto tablet:max-w-[1100px] countries-desktop:px-10 countries-desktop:max-w-full desktop:max-w-desktop desktop:px-0">
+        <header className="space-y-5 tablet:flex tablet:items-center tablet:justify-between tablet:space-y-0">
+          <SearchBox />
+          <RegionSelector />
+        </header>
+        <div className={`grid gap-5 ${navigation.state === "loading" ? 'opacity-50' : ''} transition duration-300 tablet:grid-cols-2 countries-desktop:grid-cols-3 countries-xl:grid-cols-4`}>
+          {countries.map(country => <CountryCard key={country.name.common} country={country} />).slice(0, 50)}
+        </div>
+      </div>
+    </Page>
+  )
+}
