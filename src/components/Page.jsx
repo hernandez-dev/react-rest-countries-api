4784e8c56131ef0558aecbf1e74a6e91a2c13fcc
Hernandez Maikol <hernandezdev@proton.me>
2024-03-20 12:48:34 -0400

    Components - Page - adding title default text

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -5,7 +5,7 @@ export default function Page({ title, children }) {
   // title changes
   useEffect(() => {
     window.scrollTo(0, 0)
-    document.title = title
+    document.title = 'where in de the world! - ' + title
   }, [title])
 
   return(
